83ab2a13222d8886e26016f37b88f2dc222ee3a2
PrathamGhaywat <Pratham.Ghaywat@outlook.com>
2025-09-30 09:17:17 +0200

    detects faces

--- src/face_detection.py
+++ src/face_detection.py
@@ -1,28 +1,36 @@
-import cv2
+import cv2.data
 
 def main():
-    # Open default cam 
     cap = cv2.VideoCapture(0)
 
-    if not cap.isOpened():
-        print("Webcam isn't opening")
-        return
+    # pre-trained face detector
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
     while True:
-        # Capture 
         ret, frame = cap.read()
         if not ret:
-            print("Failed")
             break
 
-        # Display 
-        cv2.imshow("snapcv", frame)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Exit with "q"
+        # Detect faces
+        faces = face_cascade.detectMultiScale(
+            gray, 
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(60, 60)
+        )
+
+        # rectangles around faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        cv2.imshow("snapcv - Face Detection", frame)
+
+        # Exit = 'q'
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    # Release and close
     cap.release()
     cv2.destroyAllWindows()
 
